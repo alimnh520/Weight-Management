bec6e26a3ce0c31cd8da91dbb4d381fc2dc2bc26
alimnh520 <alimnh412@gmail.com>
2024-04-22 22:59:20 +0600

    new container added

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// body design
+
+const container = document.querySelector(".container");
+const section = document.querySelector
+(".section");
+const slide = document.getElementById("slide");
+
+let clickSlide = true;
+
+slide.onclick = () => {
+  if (clickSlide) {
+    slide.innerHTML = "ওজন হিসাব"
+    container.style.display = "none";
+    section.style.display = "flex"
+    clickSlide = false;
+    section.classList.add("slide1");
+    container.classList.remove("slide2");
+  } else {
+    slide.innerHTML = "শতকরা হিসাব"
+    container.style.display = "flex";
+    section.style.display = "none"
+    clickSlide = true;
+    container.classList.add("slide2");
+    section.classList.remove("slide1");
+  }
+}
+
 // weight price start
 
 const weight = document.getElementById("weight");
@@ -10,7 +37,6 @@ weight.onkeyup = () => {
 }
 price.onkeyup = () => {
   result.value = (price.value * weight.value) / 1000
-  weight.value = (result.value * 1000) / price.value
 }
 result.onkeyup = () => {
   weight.value = (result.value * 1000) / price.value
@@ -23,3 +49,26 @@ clear.onclick = () => {
 }
 
 // weight price end
+// profit start
+
+const percent = document.getElementById("percentage");
+const netPrice = document.getElementById("net-price");
+const profitResult = document.getElementById("profit-result");
+const clrAll = document.getElementById("clrBtn");
+
+percent.onkeyup = () => {
+  profitResult.value = ((percent.value * netPrice.value) / 100) + parseFloat(netPrice.value);
+}
+netPrice.onkeyup = () => {
+  profitResult.value = (percent.value * netPrice.value) / 100;
+}
+profitResult.onkeyup = () => {
+  percent.value = (100 * profitResult.value) / netPrice.value
+}
+clrAll.onclick = () => {
+  percent.value = ''
+  netPrice.value = ''
+  profitResult.value = ''
+}
+
+// lrofit end
